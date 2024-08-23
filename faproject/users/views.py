40d6d77ac8acd3b5a2721cdcaa5bf23b1102bb70
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-08-23 15:27:15 +0100

    profile update

--- faproject/users/views.py
+++ faproject/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required #Added import here
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 def register(request):
@@ -16,6 +17,17 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-@login_required # Added decorator here
-def profile(request): 
-    return render(request, 'users/profile.html')
+@login_required
+def profile(request):
+    #Create instances of the forms.
+    u_form = UserUpdateForm 
+    p_form = ProfileUpdateForm
+
+    #Create a context dictionary to pass into the template. The keys are the variables
+    #we will access in our template
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context) #Context passed here
