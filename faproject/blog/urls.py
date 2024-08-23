fbdff2b995b82b685a132a26c9efe097a80d365a
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-08-23 15:56:26 +0100

    list

--- faproject/blog/urls.py
+++ faproject/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PostlistView #Changed here
+from .views import PostListView #Changed here
 from .import views
 
 urlpatterns = [
-    path('', PostlistView.as_view(), name='blog-home'), #Changed here
+    path('', PostListView.as_view(), name='blog-home'), #Changed here
     path('about/',views.about, name='blog-about'),
 ]
