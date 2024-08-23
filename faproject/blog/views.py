ddf6858f3e12b1a0b9ff8b5bed98bb32381c1acf
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-08-23 16:01:22 +0100

    ads

--- faproject/blog/views.py
+++ faproject/blog/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView
 
-class PostListView(ListView):
-	model = Post
+class PostlistView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts' #Updated here. Now the default name is set equal to 'posts'
      
 # Create your views here.x
 def home(request):
     context = {
-       'posts': Post.objects.all()
+       'posts': Post.objects.all() #The view is looping over 'posts'
     }
     return render(request, 'blog/home.html', context)
 
