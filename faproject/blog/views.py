04297a26c96972aab48354d04ed3371fc6df5be1
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-08-23 16:38:11 +0100

    create post

--- faproject/blog/views.py
+++ faproject/blog/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
-from django.views.generic import ListView, DetailView
+
+# Create your views here.x
+def home(request):
+    context = {
+       'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
-    context_object_name = 'posts' 
-    ordering = ['-date_posted'] #Change here the - will order the posts from newest to oldest.
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] 
 
 class PostDetailView(DetailView):
     model = Post  
-     
-# Create your views here.x
-def home(request):
-    context = {
-       'posts': Post.objects.all() #The view is looping over 'posts'
-    }
-    return render(request, 'blog/home.html', context)
+    
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
