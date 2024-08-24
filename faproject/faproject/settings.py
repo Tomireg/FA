816b6f566be94bb1ec5bafc338cbdc1ba9e5c838
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-08-24 13:35:49 +0100

    st

--- faproject/faproject/settings.py
+++ faproject/faproject/settings.py
@@ -120,12 +120,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-if not DEBUG:   
-    STATIC_ROOT = os.path.join(BASE_DIR, '/blog/static')
-    STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/blog/static'),
-]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
